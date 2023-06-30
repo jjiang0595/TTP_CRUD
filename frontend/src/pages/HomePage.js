7e10f125c2b79e6d920918558700eac6e5572f32
Jerry Jiang <jerryjiang49@gmail.com>
2023-06-30 19:42:32 -0400

    finish building home page

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,20 @@
+import {Link} from "react-router-dom";
+import styles from './HomePage.module.scss'
+
+function HomePage() {
+    return (
+        <div className={styles.home}>
+            <div className={styles.home__container}>
+                <h1 className={styles.home__header}>Your Stop For Campuses & Students</h1>
+                <p className={styles.home__p}>What would you like to go?</p>
+                <div className={styles.home__links}>
+                    <Link className={styles.home__links__link}>All Campuses</Link>
+                    <Link className={styles.home__links__link}>All Students</Link>
+                </div>
+            </div>
+        </div>
+    );
+
+}
+
+export default HomePage;
