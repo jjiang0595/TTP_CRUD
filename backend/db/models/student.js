1dc926202dc68e7006efeb48438a4ce1d9578274
Jerry Jiang <jerryjiang49@gmail.com>
2023-07-01 14:53:21 -0400

    adding student model

---
+++ backend/db/models/student.js
@@ -0,0 +1,35 @@
+const {DataTypes} = require('sequelize');
+const db = require('../db');
+
+const Student = db.define('Student', {
+    firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            isEmail: {
+                msg: "Invalid email address format",
+            }
+        }
+    },
+    imageUrl: {
+        type: DataTypes.STRING,
+        defaultValue: 'https://t3.ftcdn.net/jpg/04/62/93/66/360_F_462936689_BpEEcxfgMuYPfTaIAOC1tCDurmsno7Sp.jpg'
+    },
+    gpa: {
+        type: DataTypes.DECIMAL(1, 2),
+        validate: {
+            min: 0.0,
+            max: 4.0
+        }
+    }
+});
+
+module.exports = Student;
