5b776b29a70dbd4dbed0f0a26a77eabb0b606550
Jerry Jiang <jerryjiang49@gmail.com>
2023-07-01 17:12:10 -0400

    creating seeds for students & campusese

---
+++ backend/seed.js
@@ -0,0 +1,60 @@
+const {Campus, Student} = require('./db/models');
+
+const seedStudent = [
+    {
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'johndoe123@gmail.com',
+        imageUrl: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
+        gpa: 3.9
+    },
+    {
+        firstName: 'Mary',
+        lastName: 'Lee',
+        email: 'maryjane123@gmail.com',
+        imageUrl: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
+        gpa: 3.5
+    },
+    {
+        firstName: 'Jane',
+        lastName: 'Doe',
+        email: 'janedoe123@gmail.com',
+        imageUrl: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
+        gpa: 3.8
+    },
+    {
+        firstName: 'Jerry',
+        lastName: 'Jiang',
+        email: 'jerryj49@gmail.com',
+        imageUrl: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
+        gpa: 4.0
+    }
+]
+
+const seedCampus = [
+    {
+        name: 'Brooklyn College',
+        imageUrl: 'https://www.brooklyn.edu/wp-content/uploads/NEWS-Default-1-Featured.jpg',
+        address: '2900 Bedford Ave, Brooklyn, NY 11210',
+        description: 'Brooklyn College!'
+    },
+    {
+        name: 'Hunter College',
+        imageUrl: 'https://www.hunter.cuny.edu/research/repository/images/hunter_campus1.jpg/image_preview',
+        address: '695 Park Ave, New York, NY 10065',
+        description: 'Hunter College!'
+    },
+    {
+        name: 'Baruch College',
+        imageUrl: 'https://www.brooklyn.edu/wp-content/uploads/NEWS-Default-1-Featured.jpg',
+        address: '55 Lexington Ave, New York, NY 10010',
+        description: 'Baruch College!'
+    }
+];
+
+const seed = async () => {
+    await Student.bulkCreate(seedStudent);
+    await Campus.bulkCreate(seedCampus);
+};
+
+seed().then(() => process.exit());
