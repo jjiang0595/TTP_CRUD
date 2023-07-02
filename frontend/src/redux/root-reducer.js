6f9994466d2463a5b23829aa60f312d226f55774
Jerry Jiang <jerryjiang49@gmail.com>
2023-07-01 22:09:30 -0400

    fixed wrong import path

--- frontend/src/redux/root-reducer.js
+++ frontend/src/redux/root-reducer.js
@@ -1,7 +1,7 @@
 import { combineReducers } from "redux";
 
 import campusReducer from "./campus/campus.reducer";
-import studentReducer from "./campus/campus.reducer";
+import studentReducer from "./student/student.reducer";
 
 const rootReducer = combineReducers({
     campuses: campusReducer,
