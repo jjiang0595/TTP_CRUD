a6b08ccff48be2c29510ad909e5ddbd90e3f0eb4
Jerry Jiang <jerryjiang49@gmail.com>
2023-07-02 20:23:16 -0400

    created copy of studentcell to make clickable checkboxes to select campus

---
+++ frontend/src/components/campuses/SelectCampusCell.js
@@ -0,0 +1,33 @@
+import {Link} from "react-router-dom";
+import styles from './SelectCampusCell.module.scss';
+import {useDispatch} from "react-redux";
+
+function SelectCampusCell({campus, onSelect, selectedCampusId}) {
+    const dispatch = useDispatch();
+
+    const handleCheckboxChange = () => {
+        onSelect(campus.campusId);
+    };
+
+    return (
+        <li>
+            <Link to={`/campuses/${campus.campusId}`} className={styles.cell}>
+                <img src={campus.imageUrl} alt={campus.name}
+                     className={styles.cell__image}/>
+                <p className={styles.cell__text}>{campus.name}</p>
+                <Link to={`/campuses/${campus.campusId}/edit`}
+                      className={styles.cell__edit}>Edit</Link>
+            </Link>
+            <>
+                <span className={styles.cell__label}>Switch Campus</span>
+                <input
+                    type="checkbox"
+                    checked={selectedCampusId === campus.campusId}
+                    onChange={handleCheckboxChange}
+                />
+            </>
+        </li>
+    )
+}
+
+export default SelectCampusCell
