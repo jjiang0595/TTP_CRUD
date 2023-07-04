c13cf66b274b2d3beb3b2e0726d25dae948ae644
Jerry Jiang <jerryjiang49@gmail.com>
2023-07-03 22:52:17 -0400

    Update db.js

--- backend/db/db.js
+++ backend/db/db.js
@@ -2,7 +2,7 @@ const {Sequelize} = require('sequelize');
 
 const dotenv = require("dotenv");
 dotenv.config();
-
+console.log(process.env.DB_URI)
 const db = new Sequelize(process.env.DB_URI, {
     logging: false,
     dialectModule: require('pg'),
